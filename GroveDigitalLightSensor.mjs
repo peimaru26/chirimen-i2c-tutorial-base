ef7194dfed70070fd3909e49e09b3780d98a7722
植田隼平 <peimaru26@gmail.com>
2026-08-17 18:04:40 +0900

    光センサ用

---
+++ GroveDigitalLightSensor.mjs
@@ -0,0 +1,13 @@
+import { requestI2CAccess } from "node-web-i2c";
+import GROVELIGHT from "@chirimen/grove-light";
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const i2cAccess = await requestI2CAccess();
+const i2cPort = i2cAccess.ports.get(1);
+const grovelight = new GROVELIGHT(i2cPort, 0x29);
+await grovelight.init();
+while (true) {
+  const value = await grovelight.read();
+  console.log(value);
+  await sleep(200);
+}
