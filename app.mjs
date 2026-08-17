7c8d720824b76df50a19ac4d5db44099897620a9
植田隼平 <peimaru26@gmail.com>
2026-08-17 16:05:54 +0900

    チュートリアル記載のコードが微妙に古い＆今回使用するデバイスはSHT40に対応

--- app.mjs
+++ app.mjs
@@ -1,3 +1,14 @@
-import {requestI2CAccess} from "chirimen";
+import {requestI2CAccess,SHT40} from "chirimen";
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 const i2cAccess = await requestI2CAccess();
+const port = i2cAccess.ports.get(1);
+const sht40 = new SHT40(port, 0x44);
+await sht40.init();
+
+while (true) {
+  const { humidity, temperature } = await sht40.readData();
+  console.log("Humidity: "+humidity.toFixed(2)+"%,Temperature: "+temperature.toFixed(2)+"℃");
+  await sleep(500);
+}
